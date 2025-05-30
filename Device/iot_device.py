07bd32bc318f60b967e6a57cec2a7abca89ac875
forooghL <foroogh.msl@gmail.com>
2025-05-30 12:48:16 +0330

    added a simulation of IoT devices

---
+++ Device/iot_device.py
@@ -0,0 +1,28 @@
+import time
+import random
+import paho.mqtt.client as mqtt
+import json
+
+THINGSBOARD_HOST = "mqtt.eu.thingsboard.cloud"
+ACCESS_TOKEN = "hJNEJ18xj9sYDJ1B9W3t"
+
+client = mqtt.Client()
+client.username_pw_set(ACCESS_TOKEN)
+client.connect(THINGSBOARD_HOST, 1883, 60)
+client.loop_start()
+
+while True:
+    temp = round(random.uniform(20, 30), 2)
+    humidity = round(random.uniform(40, 60), 2)
+    moisture = round(random.uniform(300, 700), 2)
+
+    payload = {
+        "plant_id": random.randint(1,3),
+        "temperature": temp,
+        "humidity": humidity,
+        "moisture": moisture
+    }
+
+    client.publish("v1/devices/me/telemetry", json.dumps(payload), 1)
+    print("Sent:", payload)
+    time.sleep(10)
